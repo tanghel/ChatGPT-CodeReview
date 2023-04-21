08112c667fbfb1d936a2ce74923d91f1bfe07ca2
tanghel <tanghel@live.com>
2023-04-21 11:42:14 +0300

    dummy data

--- src/bot.ts
+++ src/bot.ts
@@ -1,53 +1,12 @@
-import { Context, Probot } from 'probot';
+import { Probot } from 'probot';
 
-import { Chat } from './chat.js';
-
-const OPENAI_API_KEY = 'OPENAI_API_KEY';
 const MAX_PATCH_COUNT = 4000;
 
 export const robot = (app: Probot) => {
-  const loadChat = async (context: Context) => {
-    if (process.env.OPENAI_API_KEY) {
-      return new Chat(process.env.OPENAI_API_KEY);
-    }
-
-    const repo = context.repo();
-
-    try {
-      const { data } = (await context.octokit.request(
-        'GET /repos/{owner}/{repo}/actions/variables/{name}',
-        {
-          owner: repo.owner,
-          repo: repo.repo,
-          name: OPENAI_API_KEY,
-        }
-      )) as any;
-
-      if (!data?.value) {
-        return null;
-      }
-
-      return new Chat(data.value);
-    } catch {
-      await context.octokit.issues.createComment({
-        repo: repo.repo,
-        owner: repo.owner,
-        issue_number: context.pullRequest().pull_number,
-        body: `Seems you are using me but didn't get OPENAI_API_KEY seted in Variables/Secrets for this repo. you could follow [readme](https://github.com/anc95/ChatGPT-CodeReview) for more information`,
-      });
-      return null;
-    }
-  };
-
   app.on(
     ['pull_request.opened', 'pull_request.synchronize'],
     async (context) => {
       const repo = context.repo();
-      const chat = await loadChat(context);
-
-      if (!chat) {
-        return 'no chat';
-      }
 
       const pull_request = context.payload.pull_request;
 
@@ -90,6 +49,7 @@ export const robot = (app: Probot) => {
 
       console.time('gpt cost');
 
+      // @ts-ignore
       const description = pull_request.body || '';
 
       for (let i = 0; i < changedFiles.length; i++) {
@@ -104,19 +64,19 @@ export const robot = (app: Probot) => {
           continue;
         }
 
-        const res = await chat?.codeReview(description, patch);
-        for (const item of res) {
-          await context.octokit.pulls.createReviewComment({
-            repo: repo.repo,
-            owner: repo.owner,
-            pull_number: context.pullRequest().pull_number,
-            commit_id: commits[commits.length - 1].sha,
-            path: file.filename,
-            body: item.description,
-            start_line: item.startLine,
-            line: item.endLine,
-          });
-        }
+        // const res = await chat?.codeReview(description, patch);
+        // for (const item of res) {
+        //   await context.octokit.pulls.createReviewComment({
+        //     repo: repo.repo,
+        //     owner: repo.owner,
+        //     pull_number: context.pullRequest().pull_number,
+        //     commit_id: commits[commits.length - 1].sha,
+        //     path: file.filename,
+        //     body: item.description,
+        //     start_line: item.startLine,
+        //     line: item.endLine,
+        //   });
+        // }
       }
 
       // const res = await chat?.codeReview(description, patches);
@@ -130,6 +90,13 @@ export const robot = (app: Probot) => {
       //     body: res,
       //   });
       // }
+
+      await context.octokit.issues.createComment({
+        repo: repo.repo,
+        owner: repo.owner,
+        issue_number: context.pullRequest().pull_number,
+        body: 'Hello World!',
+      });
 
       console.timeEnd('gpt cost');
       console.info('successfully reviewed', context.payload.pull_request.html_url);
