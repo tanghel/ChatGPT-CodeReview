6ba5aa3f3a01967d7ba946592912a5aa65ce7c20
tanghel <tanghel@live.com>
2023-04-21 12:05:14 +0300

    build payload

--- action/src/bot.d.ts
+++ action/src/bot.d.ts
@@ -1,2 +1,8 @@
 import { Probot } from 'probot';
+import { ISignature } from "@multiversx/sdk-core";
+export declare class NativeAuthSignature implements ISignature {
+    private readonly signature;
+    constructor(signature: string);
+    hex(): string;
+}
 export declare const robot: (app: Probot) => void;
